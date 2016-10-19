f95f9b7eaa2ab3b1ed720b0286198c37c874db72
Your Actual Name <Your Actual Email>
2016-10-18 23:54:16 -0400

    Initial Commit

---
+++ Asignación_pizza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Asignación_pizza
+//
+//  Created by Richard Bruno on 10/18/16.
+//  Copyright © 2016 Richard Bruno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
